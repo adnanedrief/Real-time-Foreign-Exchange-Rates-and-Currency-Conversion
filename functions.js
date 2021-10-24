580d0025cefe775e9dfffa8e5b7f957c3de5050b
Adnane Drief <adnanedrief@gmail.com>
2021-10-24 21:29:10 +0100

    reoganazied code using functions

--- functions.js
+++ functions.js
@@ -18,6 +18,34 @@ function currentTimeOfCheck() {
     return FullTime;
 }
 
-function fetchTheDataBySymbole() {
+function fetchTheDataBySymbole(Symbole) {
+    fetch('http://data.fixer.io/api/latest?access_key=8303578bb1d498a693e28e4b1edd7afb&format=1&symbols=MAD')
+        .then(response => response.json())
+        .then(data => console.log(data)); //data.rates.MAD)
 
+}
+
+function getTheElement(ID) {
+    return document.getElementById(ID);
+}
+
+function getValueOfCurrentCurrency(ID) {
+    let Current_Currency = getTheElement(ID);
+    Current_Currency.onkeyup = function() {
+        console.log(Current_Currency.value);
+    }
+    return Current_Currency.value;
+}
+
+function getValueOfDesiredCurrency(ID) {
+    let Desired_Currency = getTheElement(ID);
+    Desired_Currency.onkeyup = function() {
+        console.log(Desired_Currency.value);
+    }
+    return Desired_Currency.value;
+}
+
+function getSymboleOfCurrency(ID) {
+    let getSymboleOfCurrency = getTheElement(ID);
+    return getSymboleOfCurrency.value;
 }
