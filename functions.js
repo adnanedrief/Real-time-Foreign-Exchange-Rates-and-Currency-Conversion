ff7791458533f502830ae4c54ba40e4e837ea45e
Adnane Drief <adnanedrief@gmail.com>
2021-10-29 23:35:44 +0100

    conversion done but we need to fix to calculate formula to get a good result

--- functions.js
+++ functions.js
@@ -18,33 +18,43 @@ function currentTimeOfCheck() {
     return FullTime;
 }
 
-function fetchTheDataBySymbole(Symbole) {
-    fetch('http://data.fixer.io/api/latest?access_key=8303578bb1d498a693e28e4b1edd7afb&format=1&symbols=MAD')
-        .then(response => response.json())
-        .then(data => console.log(data)); //data.rates.MAD)
-}
-
 function getTheElement(ID) {
     return document.getElementById(ID);
 }
 
-function getValueOfCurrentCurrency(ID) {
-    let Current_Currency = getTheElement(ID);
-    Current_Currency.onkeyup = function() {
-        console.log(Current_Currency.value);
-    }
-    return Current_Currency.value;
-}
-
-function getValueOfDesiredCurrency(ID) {
-    let Desired_Currency = getTheElement(ID);
-    Desired_Currency.onkeyup = function() {
-        console.log(Desired_Currency.value);
-    }
-    return Desired_Currency.value;
-}
-
 function getSymboleOfCurrency(ID) {
-    let getSymboleOfCurrency = getTheElement(ID);
-    return getSymboleOfCurrency.value;
+    return getTheElement(ID).value;
+}
+
+function fetchTheDataBySymboleAndDoConversion(currencyOne, currencyTwo) {
+    let rateValue;
+    let formula;
+    let Current_Currency = getSymboleOfCurrency(currencyOne);
+    let Desired_Currency = getSymboleOfCurrency(currencyTwo);
+    let amountOfCurrentCurrency = getTheElement('Current_Currency');
+
+    fetch(`http://data.fixer.io/api/latest?access_key=c2ed7ca26698d1bdf75ae71a4ee1dd8f&format=1&symbols=${Current_Currency}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data); // log the json() result
+            switch (Current_Currency) {
+                case 'EUR':
+                    rateValue = data.rates.EUR;
+                    break;
+                case 'USD':
+                    rateValue = data.rates.USD;
+                    break;
+                case 'MAD':
+                    rateValue = data.rates.MAD;
+                    break;
+            }
+            formula = (amountOfCurrentCurrency.value * rateValue);
+            formula = formula.toFixed(3); //Formatting a number with exactly tree decimals
+            console.log('formula : ' + formula);
+            getTheElement('Desired_Currency').value = formula;
+
+            console.log('function.js : ' + rateValue);
+            getTheElement('rates').innerHTML = `1 ${Current_Currency} = ${rateValue} ${Desired_Currency} `;
+            // console.log(getValueOfDesiredCurrency(currencyOne));
+        });
 }
