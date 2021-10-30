6527e66773cae88ea7537f7a4c4c392f98d25265
Adnane Drief <adnanedrief@gmail.com>
2021-10-30 22:14:39 +0100

    final version

--- functions.js
+++ functions.js
@@ -26,33 +26,26 @@ function getSymboleOfCurrency(ID) {
     return getTheElement(ID).value;
 }
 
-function fetchTheDataBySymboleAndDoConversion(currencyOne, currencyTwo) {
-    let rateValue;
+function fetchDataAndDoConversion() {
+
+    let current_currency = getSymboleOfCurrency('Current_Currency_selected');
+    let desired_currency = getSymboleOfCurrency('Desired_Currency_selected');
+
+    let amountOfCurrency_One = getTheElement('Current_Currency');
+    let amountOfCurrency_Two = getTheElement('Desired_Currency');
+
+    let ratePlaceHolder = getTheElement('rates');
     let formula;
-    let Current_Currency = getSymboleOfCurrency(currencyOne);
-    let Desired_Currency = getSymboleOfCurrency(currencyTwo);
-    let amountOfCurrentCurrency = getTheElement('Current_Currency');
-    let FXRate;
-    fetch(`http://api.exchangeratesapi.io/v1/latest?access_key=4c7f7efe1bc9f06c4cf0630d7f8e0a33&format=1`)
+    let rateCalculated;
+
+    fetch("http://api.exchangeratesapi.io/v1/latest?access_key=4c7f7efe1bc9f06c4cf0630d7f8e0a33&format=1`")
         .then(response => response.json())
         .then(data => {
-            console.log(data); // get all date of all currencies
-            rateValue = data.rates[Current_Currency];
-            console.log('rateValue ' + rateValue);
-
-            console.log(data.rates[Current_Currency]); // log the json() result
-
-            FXRate = data.rates[Current_Currency] / data.rates[Desired_Currency];
-            console.log('FXRate : ' + FXRate);
-            // actually calculate the new amount
-            formula = amountOfCurrentCurrency.value * FXRate;
-
-            // formula = (amountOfCurrentCurrency.value * rateValue);
-            formula = formula.toFixed(3); //Formatting a number with exactly tree decimals
-            console.log('formula : ' + formula);
-            getTheElement('Desired_Currency').value = formula;
-
-            getTheElement('rates').innerHTML = `1 ${Current_Currency} = ${rateValue} ${Desired_Currency} `;
-            console.log('function.js : ' + rateValue);
+            console.log(data); // get all data of all currencies
+            rateCalculated = data.rates[desired_currency] / data.rates[current_currency];
+            ratePlaceHolder.innerText = `1 ${current_currency} = ${rateCalculated.toFixed(3)} ${desired_currency}`;
+            formula = amountOfCurrency_One.value * (rateCalculated);
+            amountOfCurrency_Two.value = (formula).toFixed(2);
+            // console.log(formula);
         });
 }
